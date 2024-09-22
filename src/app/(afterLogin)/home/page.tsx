2178186964a1a20abff0c3a8053ac8898e6d2019
ngeetl <ngeetl@naver.com>
2024-09-22 17:15:11 +0900

    Feat: afterLogin Home Tap, PostForm 화면 구현, TapProvider 생성

--- src/app/(afterLogin)/home/page.tsx
+++ src/app/(afterLogin)/home/page.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import Tab from './_component/Tab';
+import TabProvider from './_component/TabProvider';
+import PostForm from './_component/PostForm';
 
 const page = () => {
   return (
-    <div>홈</div>
+    <main className="w-[600px] border-[rgb(239,243,244)] border-r border-l border-solid flex flex-col items-stretch">
+      <TabProvider>
+        <Tab/>
+        <PostForm />
+        {/* <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/>
+        <Post/> */}
+      </TabProvider>
+    </main>
   )
 };
 
