3d484fa3dd13957eae03436a2c1e1cb1c3deca45
ngeetl <ngeetl@naver.com>
2024-09-25 19:10:12 +0900

    Feat: search 구현

--- src/app/(afterLogin)/search/page.tsx
+++ src/app/(afterLogin)/search/page.tsx
@@ -1,9 +1,40 @@
-import React from 'react';
+import style from './search.module.css';
+import BackButton from '../_component/BackButton';
+import SearchForm from "@/app/(afterLogin)/_component/SearchForm";
+import Tab from '../home/_component/Tab';
+import Post from '../home/_component/Post';
 
-const page = () => {
+type Props = {
+  searchParams: { q: string, f?: string, pf?: string };
+}
+export default function Search({ searchParams }: Props) {
   return (
-    <div>검색</div>
+    <main className={style.main}>
+      <div className={style.searchTop}>
+        <div className={style.searchZone}>
+          <div className={style.buttonZone}>
+            <BackButton/>
+          </div>
+          <div className={style.formZone}>
+            <SearchForm fixed={false} q={searchParams.q} />
+          </div>
+        </div>
+        <Tab/>
+      </div>
+      <div className={style.list}>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        {/*<SearchResult searchParams={searchParams} />*/}
+      </div>
+    </main>
   )
-};
-
-export default page;
+}
