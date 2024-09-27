628e6b2004497669ef7450685c7055b4c16ff61d
ngeetl <ngeetl@naver.com>
2024-09-27 23:31:44 +0900

    Feat: message view 구현

--- src/app/(afterLogin)/messages/page.tsx
+++ src/app/(afterLogin)/messages/page.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
+import style from './message.module.css';
+import Room from "@/app/(afterLogin)/messages/_component/Room";
 
-const page = () => {
+export default function Home() {
   return (
-    <div>메세지</div>
+    <main className={style.main}>
+      <div className={style.header}>
+        <h3>쪽지</h3>
+      </div>
+      <Room/>
+      <Room/>
+      <Room/>
+      <Room/>
+      <Room/>
+      <Room/>
+    </main>
   )
-};
-
-export default page;
+}
