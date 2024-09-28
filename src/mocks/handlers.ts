ef10e2993ac6eba61adc2a7f659dc58927169062
ngeetl <ngeetl@naver.com>
2024-09-28 22:08:37 +0900

    Chore: msw init

---
+++ src/mocks/handlers.ts
@@ -0,0 +1,34 @@
+import { http, HttpResponse } from 'msw'
+ 
+export const handlers = [
+    http.post('/api/login', () => {
+        return HttpResponse.json({
+            userId: 1,
+            nickname: 'kai',
+            id: 'kai3030',
+            image: '/kai_profile.jpg',
+        }, {
+            headers: {
+                'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/'
+            }
+        })
+    }),
+    http.post('/api/logout', () => {
+        return new HttpResponse(null, {
+            headers: {
+                 'Set-Cookie': 'connect.sid=;HttpOnly;Path=/Max-Age=0'
+            }
+        })
+        
+    }
+    ),
+//   // Intercept "GET https://example.com/user" requests...
+//   http.get('https://example.com/user', () => {
+//     // ...and respond to them using this JSON response.
+//     return HttpResponse.json({
+//       id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
+//       firstName: 'John',
+//       lastName: 'Maverick',
+//     })
+//   }),
+]
