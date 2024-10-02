f87a296647cf5256e3ae3f83f2293d4657b61f66
ngeetl <ngeetl@naver.com>
2024-10-02 16:28:07 +0900

    Feat: onSubmit server 컴포넌트를 따로 분리하여 signup페이지를 client 컴포넌트로 바꿈, 유효성 체크 함수 생성

--- src/app/(beforeLogin)/_component/signup.tsx
+++ src/app/(beforeLogin)/_component/signup.tsx
@@ -1,45 +1,37 @@
+"use client";
 
 import BackButton from "@/app/(afterLogin)/_component/BackButton";
 import ModalInput from "@/components/modal/ModalInput";
-import { redirect } from "next/navigation";
+import onSubmit from '../_lib/signup';
+import { useFormState, useFormStatus } from 'react-dom';
 
+
+const showMessage = (messasge: string | null | undefined) => {
+  if (messasge === 'no_id') {
+    return '아이디를 입력하세요.';
+  }
+  if (messasge === 'no_name') {
+    return '닉네임을 입력하세요.';
+  }
+  if (messasge === 'no_password') {
+    return '비밀번호를 입력하세요.';
+  }
+  if (messasge === 'no_image') {
+    return '이미지를 업로드하세요.';
+  }
+  if (messasge === 'user_exists') {
+    return '이미 사용 중인 아이디입니다.';
+  }
+  return '';
+};
 
 const Signup = () => {
-  const submit = async (FormData: FormData) => {
-    "use server"
+  const [state, formAction] = useFormState(onSubmit, { message: null });
+  const { pending } = useFormStatus();
 
-    // form 유효성 검사
-    if(!FormData.get('id')) return { message: 'no_id' };
-    if(!FormData.get('name')) return { message: 'no_name' };
-    if(!FormData.get('password')) return { message: 'no_password' };
-    if(!FormData.get('image')) return { message: 'no_image' };
+  // style
+  const inputClassName = "w-full h-full mt-3 text-base p-3 outline-none";
 
-    let shouldRedirect = false;
-
-    try {
-      const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/users`, {
-        method: 'POST',
-        body: FormData,
-        credentials: 'include' // 로그인한 유저가 또 회원가입 하는 것을 방지하기 위함
-      });
-
-      console.log(response.status);
-
-      // usr가 이미 존재하는 지 확인
-      if(response.status === 403) return { message: 'user_exists' };
-
-      console.log(await response.json());
-
-      shouldRedirect = true;
-    } catch (err) {
-      console.error(err);
-      return
-    }
-
-    if(shouldRedirect) redirect('/home');
-  }
-
-  const inputClassName = "w-full h-full mt-3 text-base p-3 outline-none"
   return (
     <div className="w-dvw h-full flex justify-center absolute bg-black bg-opacity-40 top-0 left-0 right-0 bottom-0">
       <div className=" bg-white p-4 relative top-[5%] max-w-[80vw] min-w-[600px] rounded-2xl flex flex-col h-[550px]">
@@ -48,7 +40,7 @@ const Signup = () => {
           <BackButton/>
         </div>
 
-        <form action={submit} className="h-full flex flex-col justify-around" >
+        <form action={formAction} className="h-full flex flex-col justify-around" >
           <div className="flex-1 py-0 px-[80px]">
             <div className="text-2xl font-bold mb-8">계정을 생성하세요.</div>
 
@@ -62,10 +54,10 @@ const Signup = () => {
                   required
                 />
             </ModalInput>
-            <ModalInput title="닉네임" id="nickname">
+            <ModalInput title="네임" id="name">
               <input
-                  id="nickname"
-                  name="nickname"
+                  id="name"
+                  name="name"
                   className={inputClassName}
                   type="text"
                   placeholder=""
@@ -94,9 +86,10 @@ const Signup = () => {
             </ModalInput>
           </div>
           <div className="py-6 px-20">
-            <button type="submit" className="w-full h-11 rounded-3xl text-white bg-gray-500 hover:bg-gray-700 cursor-pointer transition">
+            <button disabled={pending} type="submit" className="w-full h-11 rounded-3xl text-white bg-gray-500 hover:bg-gray-700 cursor-pointer transition">
             가입하기
             </button>
+            <div className="text-red-500 font-bold mt-2 text-sm text-center">{showMessage(state?.message)}</div>
         </div>
         </form>
       </div>
